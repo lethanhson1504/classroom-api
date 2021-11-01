b9104bc5288b5597d874119e3e7720198a1d3c27
le thanh son <lethanhson1504@gmail.com>
2021-11-01 17:08:31 +0700

    changing .env file

--- src/datasources/d-bdatasource.datasource.ts
+++ src/datasources/d-bdatasource.datasource.ts
@@ -5,11 +5,11 @@ const config = {
   name: 'DBdatasource',
   connector: 'postgresql',
   url: 'postgres://ztybisanhtxeqh:8e512a43c69934ba3e1732a67d1b70136e58fc8218bcb26a4caa92942e339b46@ec2-18-213-179-70.compute-1.amazonaws.com:5432/df10ldbanqt1k1',
-  // host: 'localhost',
-  // port: 5432,
-  // user: 'postgres',
-  // password: 'postgres',
-  // database: 'btcn_classroom_dev'
+  host: 'ec2-18-213-179-70.compute-1.amazonaws.com',
+  port: 5432,
+  user: 'ztybisanhtxeqh',
+  password: '8e512a43c69934ba3e1732a67d1b70136e58fc8218bcb26a4caa92942e339b46',
+  database: 'df10ldbanqt1k1'
 };
 
 // Observe application's life cycle to disconnect the datasource when
